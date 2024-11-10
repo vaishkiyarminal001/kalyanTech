b9f0b30aee51eb0e60a660e90b131b6673dc6df4
vaishkiyarminal001 <vaishkiyarminal001@gmail.com>
2024-11-10 22:11:37 +0530

    Changes done in navbar and  home page

--- src/Component/Pages/Home.jsx
+++ src/Component/Pages/Home.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
-import './Home.css'; 
+import './Home.css';
 import { Carousels } from './Carousels';
-
+import Marquee from 'react-fast-marquee';
 
 export const Home = () => {
   return (
     <div className="home-container">
+      <Marquee direction="left" speed={60} delay={10} style={{ height: '50px', width: '100%', margin: 'auto', }}>
+        <h1 className="marquee-text">
+          Empower your future with <span className="highlight">"Kalyan Tech"</span>
+        </h1>
+      </Marquee>
       
-      {/* Carousal Section */}
-
-     <Carousels/>
-
-        {/* Tagline Section */}
-        <h1 className="home-tagline">
-        Empower your future with <span className="highlight">Kalyan Tech</span>
-      </h1>
-    
+      {/* Carousel Section */}
+      <Carousels />
     </div>
   );
 };
