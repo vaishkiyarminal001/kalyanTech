3e124acb8b9ac018dd5638ea946abed9411d1a89
vaishkiyarminal001 <vaishkiyarminal001@gmail.com>
2024-11-10 19:11:57 +0530

    Navbar ajdusted

---
+++ src/Component/Pages/Carousels.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Carousel } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export const Carousels = () => {
+  return (
+    <div>Carousels</div>
+  )
+}
--- src/Component/Pages/Home.jsx
+++ src/Component/Pages/Home.jsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import './Home.css'; // Import the CSS file
-import homeVideo from "../Assets/homevid.mp4"; // Import the video from Assets folder
+import './Home.css'; 
+import { Carousels } from './Carousels';
+
 
 export const Home = () => {
   return (
     <div className="home-container">
-      {/* <h1 className="home-title">Welcome to Home Page</h1> */}
+      
+      {/* Carousal Section */}
 
-      {/* Video Section */}
-      {/* <div className="video-container">
-        <video className="home-video" autoPlay loop muted>
-          <source src={homeVideo} type="video/mp4" />
-        </video>
-      </div> */}
-
+     <Carousels/>
 
         {/* Tagline Section */}
         <h1 className="home-tagline">
         Empower your future with <span className="highlight">Kalyan Tech</span>
       </h1>
-      
+    
     </div>
   );
 };
